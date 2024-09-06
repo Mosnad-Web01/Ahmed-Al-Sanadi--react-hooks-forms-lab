bc47275ac7ae1b23cccc87fa2b2d5841ead7e451
Ahmed Alsanadi <ahmed11alsanadi11@gmail.com>
2024-09-06 05:36:40 +0300

    onSearchChange Functiion has been added successfully

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,23 +3,34 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, onItemFormSubmit }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState("");
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
-
-    return item.category === selectedCategory;
-  });
+	const itemsToDisplay = items
+		// category
+		//filter out all items if selectedCategory is 'All' or filter out items that don't match the selected category
+		.filter(
+			(item) =>
+				selectedCategory === 'All' ||
+				item.category === selectedCategory,
+		) // search term filter - filter out the filtered items that don't match the search term
+		.filter((item) =>
+			item.name.toLowerCase().includes(search.toLowerCase()),
+		);
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={onItemFormSubmit} />
+      <Filter
+        search={search}
+        onSearchChange={setSearch}
+        onCategoryChange={handleCategoryChange}
+      />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
