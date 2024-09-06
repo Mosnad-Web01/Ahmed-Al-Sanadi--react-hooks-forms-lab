f9a8c0638ca8f575776636f5554399df7a8e8af6
Ahmed Alsanadi <ahmed11alsanadi11@gmail.com>
2024-09-06 06:09:38 +0300

    Added the functionality of (Add to list button)

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React from 'react';
 
-function Filter({ onCategoryChange , onSearchChange }) {
-
-  return (
-    <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." onChange={(e) => onSearchChange(e.target.value)}/>
-      <select name="filter" onChange={onCategoryChange}>
-        <option value="All">Filter by category</option>
-        <option value="Produce">Produce</option>
-        <option value="Dairy">Dairy</option>
-        <option value="Dessert">Dessert</option>
-      </select>
-    </div>
-  );
+function Filter({ onCategoryChange, onSearchChange, search }) {
+	return (
+		<div className="Filter">
+			<input
+				type="text"
+				value={search}
+				name="search"
+				placeholder="Search..."
+				onChange={(e) => onSearchChange(e.target.value)}
+			/>
+			<select name="filter" onChange={onCategoryChange}>
+				<option value="All">Filter by category</option>
+				<option value="Produce">Produce</option>
+				<option value="Dairy">Dairy</option>
+				<option value="Dessert">Dessert</option>
+			</select>
+		</div>
+	);
 }
 
 export default Filter;
